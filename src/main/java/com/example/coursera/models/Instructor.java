d388204a852b6bae8d6da6c67da52e7a8c9bbdbd
Boris Nikolov <nikolov_b@abv.bg>
2022-11-25 09:40:10 +0200

    Add Instructor

---
+++ src/main/java/com/example/coursera/models/Instructor.java
@@ -0,0 +1,58 @@
+package com.example.coursera.models;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "instructors")
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "instructor_id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "time_created")
+    private LocalDate timeCreated;
+
+    public Instructor() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(LocalDate timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+}
